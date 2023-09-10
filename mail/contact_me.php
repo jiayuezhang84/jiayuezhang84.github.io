797dc00e4e762d36240944f3386c26279a414988
jiayuezhang84 <58899463+jiayuezhang84@users.noreply.github.com>
2023-09-10 01:38:32 -0400

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,7 +22,7 @@ $message = $_POST['message'];
 
 if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) don't send!
     // Create the email and send the message
-    $to = 'https://formspree.io/f/xqkvbvlp'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'josiezhang84@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@josiezhang.github.io\n";	       
